1fb3d3393b9b2e29df492cf98ce3247f811b3a66
Audrius Dailyde <audrius.dailyde@revelsystems.com>
2020-04-14 08:35:38 +0300

    button for action dispatch

--- structure-m/src/app/components/sample/SamplePage.tsx
+++ structure-m/src/app/components/sample/SamplePage.tsx
@@ -1,17 +1,30 @@
 import React, { FC } from "react";
 import SampleTable from "./SampleTable";
+import { addSample } from "../../redux/actions";
+import { connect } from "react-redux";
 
-interface IProps {}
+interface IProps {
+  addSample: (sample: any) => void;
+}
 
 const SamplePage: FC<IProps> = (props) => {
+  const onButtonClick = () => {
+    props.addSample({ name: "new sample", description: "new description" });
+  };
+
   return (
     <div className="bottom">
       <h1>{"Sample:"}</h1>
       <SampleTable></SampleTable>
+      <button onClick={onButtonClick}>Add sample</button>
     </div>
   );
 };
 
 SamplePage.defaultProps = {};
 
-export default SamplePage;
+const mapDispatchToProps = {
+  addSample,
+};
+
+export default connect(null, mapDispatchToProps)(SamplePage);
